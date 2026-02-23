17ec191c9ebda84d85c7fca1cfe5917cdb4aa4f7
h4rdz <khuongduyle.it@gmail.com>
2026-02-23 14:17:21 +0700

    feat: implement jwt sign and verify method + happy new yearrr

--- node-day-6/practice/src/middlewares/auth.middleware.js
+++ node-day-6/practice/src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const jwt2 = require("../utils/jwt2")
 const { secret } = require("../configs/jwt");
 const userModel = require("../models/user.model");
 
@@ -11,25 +12,22 @@ const auth = async (req, res, next) => {
     }
 
     const accessToken = bearerToken.split(" ")[1];
-
-    const payload = jwt.verify(accessToken, secret);
-
+    const payload = jwt2.verify(accessToken, secret);
     const currentUser = await userModel.findById(payload.sub);
 
     if (!currentUser) {
-      throw Error();
+      const error = new Error()
+      error.message = "resource not found";
+      error.statusCode = 404;
+      throw error;
     }
 
-    req.user = currentUser; 
-    req.user.id = payload.sub; 
+    req.user = currentUser;
+    req.user.id = payload.sub;
 
     next();
   } catch (error) {
-    if (error.name === "TokenExpiredError") {
-      return res.error("Token has expired", 401);
-    }
-
-    return res.error("Unauthorized: " + error.message, 401);
+    return res.error("Unauthorized: " + error.message, error.statusCode || 401);
   }
 };
 
--- node-day-6/practice/src/services/auth.service.js
+++ node-day-6/practice/src/services/auth.service.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
+const { secret } = require("../configs/jwt");
+const jwt2 = require("../utils/jwt2");
 
 class AuthService {
   model = userModel;
@@ -18,14 +20,24 @@ class AuthService {
     const user = await userModel.findByEmailAndPassword(email, password);
     if (!user) return null;
 
-    const { secret } = require("../configs/jwt");
-    const ttl = "1h";
+    const header = {
+      alg: "HS256",
+      typ: "JWT",
+    };
+     
+    const payload = {
+      sub: user.id,
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000 + 300), // 5phut
+    };
 
-    const token = jwt.sign({ sub: user.id }, secret, { expiresIn: ttl });
+    const token2 = jwt2.sign(header, payload, secret);
+
+    const token = jwt.sign({ sub: user.id }, secret, { expiresIn: "5m" });
 
     return {
       user,
-      loginToken: { access_token: token, access_token_ttl: 3600 },
+      loginToken: { access_token: token2, access_token_ttl: 3600 },
     };
   }
 }
